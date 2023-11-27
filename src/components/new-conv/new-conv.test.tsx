f6955826ac6f39fb355deffabff715a5b7001371
GeorgeNonis <georgenonis@gmail.com>
2023-11-27 23:31:44 +0100

    checking for image as well

--- src/components/new-conv/new-conv.test.tsx
+++ src/components/new-conv/new-conv.test.tsx
@@ -1,14 +1,19 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import NewConversation from ".";
 
 describe("NewConv", () => {
-  it("Renders MeowGPT title,the icon and the picture", () => {
+  it("Renders MeowGPT title,the icon and the picture", async () => {
     render(<NewConversation />);
 
     // Check if image is rendered
-    const image = screen.getByAltText("MeowGPT");
-    expect(image).toBeInTheDocument();
+    await waitFor(() => {
+      const image = screen.getByAltText("MeowGPT");
+      expect(image).toHaveAttribute(
+        "src",
+        expect.stringContaining("/_next/image?url=%2Fcat.png&w=64&q=75")
+      );
+    });
 
     // Check if text is rendered
     expect(screen.getByAltText("MeowGPT")).toBeInTheDocument();
