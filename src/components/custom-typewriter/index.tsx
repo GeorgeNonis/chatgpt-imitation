f53c2d2a73d3e2c8e1d7b05c3f2e90d8aede3a75
GeorgeNonis <georgenonis@gmail.com>
2023-11-29 23:44:26 +0100

    apply useTypwriter

--- src/components/custom-typewriter/index.tsx
+++ src/components/custom-typewriter/index.tsx
@@ -1,6 +1,8 @@
+import useTypewriter from "../../../hooks/typewriter";
 import { StyledTypewriter } from "./custom-typewriter.styles";
 
 const Typewriter = () => {
+  const {} = useTypewriter();
   return <StyledTypewriter></StyledTypewriter>;
 };
 export default Typewriter;
