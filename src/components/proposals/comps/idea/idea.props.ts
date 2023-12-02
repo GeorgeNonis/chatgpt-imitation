500f2b01d171737582e35dcd4b3c28ba31b701cd
GeorgeNonis <georgenonis@gmail.com>
2023-12-02 22:49:19 +0100

    passing the handler

--- src/components/proposals/comps/idea/idea.props.ts
+++ src/components/proposals/comps/idea/idea.props.ts
@@ -1,5 +1,7 @@
+import { Attributes, PropsWithoutRef, RefAttributes } from "react";
+
 export type IdeaI = {
   title: string;
   idea: string;
   ["data-testid"]?: string;
-};
+} & React.HtmlHTMLAttributes<HTMLDivElement>;
